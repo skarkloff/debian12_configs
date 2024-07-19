64d9aaaa3594cbc65c7b40958f94456f89e4e45f
lautarosk <skarkloff.ls99@gmail.com>
2024-07-18 22:52:49 -0300

    agregando qtile y autostart 2

---
+++ autostart.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+###########################################################
+# polkit para abrir apps que necesitan sudo
+# policykit-1-gnome 
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 @
